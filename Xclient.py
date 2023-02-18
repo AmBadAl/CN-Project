8d69b48fefc635d3e92f7dd59ded40ba1e98c386
Amirreza <amirreza.bagheri7680@gmail.com>
2023-02-18 19:13:43 +0330

    Xclient v1

--- Xclient.py
+++ Xclient.py
@@ -1,119 +1,62 @@
-import multiprocessing as mp
-import socket
-import logging
-import numpy as np
-from queue import Queue
-import ssl
-import time
-import sys
-import argparse
-import random
-import time
+import socket, threading
 
 
+# Receive from client
+def receive_from_client():
+    # setting parameters
+    local_IP = "127.0.0.1"
+    local_port = 20001
+    buffer_size = 1024
+    # Create a datagram socket
+    global UDPServerSocket
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # Bind to address and ip
+    UDPServerSocket.bind((local_IP, local_port))
+    print("UDP server up and listening")
+    # Listen for incoming datagrams
+    while True:
+        bytesAddressPair = UDPServerSocket.recvfrom(buffer_size)
+        data = bytesAddressPair[0]
+        address = bytesAddressPair[1]
+        # convert data = {'message': message, 'server_addr': server_addr}
+        # extract new address and message and send the result
+        send_to_xserver(data['message'], data['server_addr'], address)
 
 
+# Send to Xserver
+def send_to_xserver(message, server_addr, client_addr):
+    # send {message, client_address, server_addr} to xserver
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST_XSERVER, PORT_XSERVER))
+        s.sendall(str({'message': message, 'server_addr': server_addr, 'client_addr': client_addr}).encode())
 
 
-def parse_input_argument():
-    parser = argparse.ArgumentParser(description='This is a client program that create a tunnel\
-                                                  to the server over various TCP connections.')
-
-    parser.add_argument('-ut', '--udp-tunnel', action='append', required=True,
-                        help="Make a tunnel from the client to the server. The format is\
-                              'listening ip:listening port:remote ip:remote port'.")
-    parser.add_argument('-s', '--server', required=True,
-                        help="The IP address and (TCP) port number of the tunnel server.\
-                               The format is 'server ip:server port'.")
-    parser.add_argument('-v', '--verbosity', choices=['error', 'info', 'debug'], default='info',
-                        help="Determine the verbosity of the messages. The default value is 'info'.")
-
-    args = parser.parse_args()
-    return args
-
-
-
-def read_n_byte_from_tcp_sock(sock, n):
-    '''Just for read n byte  from tcp socket'''
-    buff = bytearray(n)
-    pos = 0
-    while pos < n:
-        cr = sock.recv_into(memoryview(buff)[pos:])
-        if cr == 0:
-            raise EOFError
-        pos += cr
-    return buff
+# receive from xserver
+def receive_to_xserver():
+    # receive {message, client_address, server_address} from xserver
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST_XCLIENT, PORT_XCLIENT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr} : receive_to_xserver")
+            while True:
+                # we assume data = {'message': message, 'client_addr': (client_host, client_port)}
+                data = conn.recv(1024)
+                print(data)
+                data = dict(data)
+                send_to_client(data['message'], data['client_addr'][0], data['client_addr'][1])
+                # pass the message to the proper client
 
 
-
-def handle_tcp_conn_recv(stcp_socket, udp_socket, incom_udp_addr):
-    '''
-    read from tcp socket for the UDP segment received through the tunnel,
-    then forward received segment to incom_udp_addr
-    '''
- 
+def send_to_client(message, server_address, client_address):
+    UDPServerSocket.sendto(str.encode(str({'message': message, 'address': server_address})), client_address)
 
 
-def handle_tcp_conn_send(stcp_socket, rmt_udp_addr, udp_to_tcp_queue):
-    '''
-     get remote UDP ip and port(rmt_udp_addr) and Concat them then sending it to the TCP socket
-    after that read from udp_to_tcp_queue for sendeig a UDP segment and update queue,
-    don't forgot to block the queue when you are reading from it.
-    '''
+threading.Thread(target=receive_from_client).start()
+threading.Thread(target=receive_to_xserver).start()
 
-
-
-
-
-
-def handle_udp_conn_recv(udp_socket, tcp_server_addr, rmt_udp_addr):
-    '''
-        This function should be in while True,
-        Receive a UDP packet form incom_udp_addr.
-        It also keeps the associated thread for handling tcp connections in udp_conn_list,
-        if incom_udp_addr not in udp_conn_list, Recognize a new UDP connection from incom_udp_addr. So establish a TCP connection to the remote server for it
-        and if incom_udp_addr in udp_conn_list you should continue sending in esteblished socekt  ,
-        you need a queue for connecting udp_recv thread to tcp_send thread.
-         '''
-        pass
-
-if __name__ == "__main__":
-    args = parse_input_argument()
-    
-    tcp_server_ip = args.server.split(':')[0]
-    tcp_server_port = int(args.server.split(':')[1])
-    tcp_server_addr = (tcp_server_ip, tcp_server_port)
-    
-    if args.verbosity == 'error': log_level = logging.ERROR
-    elif args.verbosity == 'info': log_level = logging.INFO
-    elif args.verbosity == 'debug': log_level = logging.DEBUG
-    format = "%(asctime)s: (%(levelname)s) %(message)s"
-    logging.basicConfig(format=format, level=log_level, datefmt="%H:%M:%S")
-
-    for tun_addr in args.udp_tunnel:
-        tun_addr_split = tun_addr.split(':')
-        udp_listening_ip = tun_addr_split[0]
-        udp_listening_port = int(tun_addr_split[1])
-        rmt_udp_ip = tun_addr_split[2]
-        rmt_udp_port = int(tun_addr_split[3])
-        rmt_udp_addr = (rmt_udp_ip, rmt_udp_port)
-
-        try:
-            udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-            udp_socket.bind((udp_listening_ip, udp_listening_port))
-        except socket.error as e:
-            logging.error("(Error) Error openning the UDP socket: {}".format(e))
-            logging.error("(Error) Cannot open the UDP socket {}:{} or bind to it".format(udp_listening_ip, udp_listening_port))
-            sys.exit(1)
-        else:
-            logging.info("Bind to the UDP socket {}:{}".format(udp_listening_ip, udp_listening_port))
-    
-        mp.Process(target=handle_udp_conn_recv,
-                   args=(udp_socket, tcp_server_addr, rmt_udp_addr)).start()
-
-
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        logging.info("Closing the TCP connection...")
+HOST_XSERVER = "127.0.0.1"
+PORT_XSERVER = 65432
+HOST_XCLIENT = "127.0.0.1"
+PORT_XCLIENT = 21354
